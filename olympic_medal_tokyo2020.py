3c9504fe1029bee4a51fd779617b295350bb5feb
Maria <maria@Marias-MacBook-Air.local>
2021-08-24 22:18:59 +0200

    adding functions to the dashboard code

--- olympic_medal_tokyo2020.py
+++ olympic_medal_tokyo2020.py
@@ -19,45 +19,27 @@ olympic_medal_map['id'] = country_codes['Numeric']
 #Reading data by gender
 medal_count_by_gender = pd.read_csv('medal_count_by_gender.csv',sep=',', encoding='latin-1')
 
+###Drawing a map
 
-###Title and initial text
-
-st.image('logo.png', width=100)
-st.title('Tokyo Olympics 2020')
-st.write('')
-st.write('This dashboard includes data from <a href="https://olympics.com/tokyo-2020/olympic-games/en/results/all-sports/medal-standings.htm">Tokyo 2020 Olympics Website</a>. You can switch between two views in the side menu: total medals by country or check statistics by gender',unsafe_allow_html=True)
-
-st.write('')
-
-###Sidebar menu
-
-MODE = st.sidebar.radio('Select view',['Total Medals by country','Statistics by by gender'])
-
-
-###Map
-
-if MODE == 'Total Medals by country':
-    
-    MEDALS = st.selectbox('Type of medals',
-               options = ['Total','Gold','Silver','Bronze'])
+def draw_map(mtype='Total'):
     
     COLOR_THEME = {'Total':"lightgreyred",
                    'Gold':"lightorange",
                    'Silver':"lightgreyteal",
                    'Bronze':"lightgreyred"}
     
-    olympic_medal_map['Medals'] = olympic_medal_map[MEDALS]
+    olympic_medal_map['Medals'] = olympic_medal_map[mtype]
 
     source = alt.topo_feature(data.world_110m.url, "countries")
 
     background = alt.Chart(source).mark_geoshape(fill="white")
 
     foreground = (
-        alt.Chart(source, title=f'Countries by number of {MEDALS} medals')
+        alt.Chart(source, title=f'Countries by number of {mtype} medals')
         .mark_geoshape(stroke="black", strokeWidth=0.15)
         .encode(
             color=alt.Color(
-                "Medals:N", scale=alt.Scale(scheme=COLOR_THEME[MEDALS]), legend=None
+                "Medals:N", scale=alt.Scale(scheme=COLOR_THEME[mtype]), legend=None
             ),
             tooltip=[
                 alt.Tooltip("Team/NOC:N", title="Team"),
@@ -76,34 +58,69 @@ if MODE == 'Total Medals by country':
         .properties(width=600, height=400)
         .project("naturalEarth1")
     )
+    
+    return final_map
 
-    st.write(final_map)
+### Medals by gender
+
+def medals_by_gender(countries=['Japan']):
+    chart = alt.Chart(medal_count_by_gender.loc[(medal_count_by_gender['Medal type'] != 'Total')&(medal_count_by_gender['Team/NOC'].isin(COUNTRY))], title='Total medals earned by female and male athlets').mark_circle(size=300).encode(
+        x=alt.X('count_male', title='Male Athlets'),
+        y=alt.Y('count_female', title='Female Athlets'),
+        color=alt.Color('Medal type',
+                        scale=alt.Scale(domain=['Gold','Silver','Bronze'],
+                                        range=['orange','grey','brown'])),
+        tooltip=['Team/NOC', 'Medal type','count_female','count_male']
+    ).properties(
+        width=600,
+        height=400
+    )
+    
+    line = alt.Chart(pd.DataFrame({'var1': [0, 30], 'var2': [0, 30]})
+                    ).mark_line(color='grey',
+                                opacity=.3,
+                                strokeDash=[5,5]
+                               ).encode(alt.X('var1'),
+                                        alt.Y('var2')
+                                       )
+    return chart+line
+
+#---------- STREAMLIT APP ---------------
+
+#Display page title and some text about the app
+st.image('logo.png', width=100)
+st.title('Tokyo Olympics 2020')
+st.write('')
+st.write('This interactive dashboard includes data from <a href="https://olympics.com/tokyo-2020/olympic-games/en/results/all-sports/medal-standings.htm">Tokyo 2020 Olympics Website</a>. You can switch between two views in the side menu: total medals by country or check statistics by gender',unsafe_allow_html=True)
+
+#Display sidebar menu to choose between two views
+
+MODE = st.sidebar.radio('Select view',['Total Medals by country','Statistics by by gender'])
+
+#If Map view was chosen
+
+if MODE == 'Total Medals by country':
+    
+    #add select for type of medal. The first one in options is the default value
+    MEDALS = st.selectbox('Type of medals',
+               options = ['Total','Gold','Silver','Bronze'])
+    
+
+    #display the map
+    st.write(draw_map(MEDALS))
+    #display the datatable below the map
     st.table(olympic_medal_map.reset_index().set_index('Team/NOC')[['Medals']].sort_values(by='Medals', ascending=False))
 
-### Split by gender
+#If split by gender was chosen
 else:
     
+    #add select option for countries with multiple choice. Default values would be Japan, ROC and Sweden
     COUNTRY = st.multiselect('Select a team',
                list(medal_count_by_gender['Team/NOC'].drop_duplicates().values),
                             ['Japan','ROC','Sweden'])
     
-    chart = alt.Chart(medal_count_by_gender.loc[(medal_count_by_gender['Medal type'] != 'Total')&(medal_count_by_gender['Team/NOC'].isin(COUNTRY))], title='Total medals earned by female and male athlets').mark_circle(size=300).encode(
-    x=alt.X('count_male', title='Male Athlets'),
-    y=alt.Y('count_female', title='Female Athlets'),
-    color=alt.Color('Medal type', 
-                    scale=alt.Scale(domain=['Gold','Silver','Bronze'],range=['orange','grey','brown'])),
-    tooltip=['Team/NOC', 'Medal type','count_female','count_male']
-).properties(
-    width=600,
-    height=400
-)
-    line = alt.Chart(
-    pd.DataFrame({'var1': [0, 30], 'var2': [0, 30]})).mark_line(color='grey',opacity=.3,strokeDash=[5,5]).encode(
-            alt.X('var1'),
-            alt.Y('var2'),
-)
-    
-    st.write(chart+line)
+    #display the graph and table with the results
+    st.write(medals_by_gender(COUNTRY))
     st.write('Medals won by selected countries')
     st.table(medal_count_by_gender.loc[medal_count_by_gender['Team/NOC'].isin(COUNTRY),['Team/NOC','Medal type','count_female','count_male']])
     
